f051187f574d66dc0b9974a448b0f3fdd50eb55f
nadounegi <it22083027@tsb-yyg.ac.jp>
2023-04-15 13:03:31 +0900

    更新

--- files/java_sql.sql
+++ files/java_sql.sql
@@ -1,18 +1,16 @@
-select * from gsales;
-select * from goods;
-select * from salesman;
+select * from GSALES_mst;
+select * from GOODS_mst;
+select * from Salesman_mst;
 
 select GNAME,GPRICE,GNUM,allSum
 
-from goods,(select GID as salesid,sum(SNUM) as allSum
-			from gsales
-			where trunc(SDATE) = trunc(Sysdate)
+from GOODS_mst,(select GID as salesid,sum(SNUM) as allSum
+			from GSALES_mst
+			where DATE(SDATE) = CURDATE()
 			group by GID)
 where GID = salesid;
 
-select trunc(Sydate) from dual;
-select trunc(Sydate) +1 from dual;
-select Sydate from dual;
-
-select SID, to_char(SDATE,'yyyy/mm/dd') from gsales;
+select CURDATE();
+select DATE_ADD(CURDATE(), INTERVAL 1 DAY);
+SELECT SID, DATE_FORMAT(SDATE,'%Y/%m/%d') AS 'Sale Date' from GSALES_mst;
 		
